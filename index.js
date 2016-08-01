c2fbf2a3270940c756025a39977129fcce18e742
Scott Kirkland <srkirkland@gmail.com>
2016-08-01 15:21:09 -0700

    api

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/api', function(req,res){
+  res.json(require('./nams.json'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
